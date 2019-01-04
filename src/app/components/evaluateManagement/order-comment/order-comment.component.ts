351d26db5c94b8dc21ca458c4922132df01b50b8
鄭紅霞 <zheng.hongxia@realsys.co.jp>
2019-01-04 18:31:17 +0800

    '创建组件'

---
+++ src/app/components/evaluateManagement/order-comment/order-comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-comment',
+  templateUrl: './order-comment.component.html',
+  styleUrls: ['./order-comment.component.scss']
+})
+export class OrderCommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/evaluateManagement/user-level/user-level.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-level',
+  templateUrl: './user-level.component.html',
+  styleUrls: ['./user-level.component.scss']
+})
+export class UserLevelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
